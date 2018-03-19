d6c19477625668bfba0fbddc04d1098ce5d86e13
yu <hengchen2005@gmail.com>
2018-03-19 23:46:51 +0800

    commit 20180319234645

--- flash_get.py
+++ flash_get.py
@@ -12,8 +12,12 @@
 """
 
 
+import os
+import random
 from urllib import request
 from urllib import parse
+
+from http_download import HttpDownLoader
 
 class FlashGet(object):
 
@@ -35,6 +39,7 @@ class FlashGet(object):
         ssl._create_default_https_context = ssl._create_unverified_context
         proxy_opener = request.ProxyHandler({'https': '127.0.0.1:8087'})
         opener = request.build_opener(proxy_opener)
+        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
         request.install_opener(opener)
         return opener
 
@@ -45,6 +50,13 @@ class FlashGet(object):
         length = headers['Content-Length']
         return name, path, length
 
+
+    def create_tmp_file(self, path, size):
+        with open(path, 'wb') as f:
+            f.seek(size - 1)
+            f.write(b'\x00')
+
+
 if __name__ == '__main__':
     url = 'http://issuecdn.baidupcs.com/issue/netdisk/yunguanjia/BaiduNetdisk_6.0.2.exe'
     # url = 'https://dldir1.qq.com/qqfile/qq/QQ9.0.1/23161/QQ9.0.1.exe'
@@ -52,3 +64,28 @@ if __name__ == '__main__':
     # url='https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7BA11612D1-4FA0-F3F8-B6FB-30F3918552C5%7D%26lang%3Dzh-CN%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dstable-arch_x86-statsdef_1%26installdataindex%3Ddefaultbrowser/chrome/install/ChromeStandaloneSetup.exe'
     fg = FlashGet(url)
     name, path, length = fg.pase_url(url)
+    length = int(length)
+    # fg.create_tmp_file(name, length)
+    print('length={}'.format(length))
+    max_thread = 10
+    cal = divmod(length, max_thread)
+    if cal[1] == 0:
+        block_size = cal[0]
+        for i in range(0, max_thread):
+            downloader = HttpDownLoader(fg.get_opener(), url, i*block_size, (i+1)*block_size, name)
+            downloader.start()
+    else:
+        block_size = divmod(length, max_thread+1)[0]
+        print('block_size={}'.format(block_size))
+        for i in range(0, max_thread):
+            if i < max_thread - 1:
+                print('i={}, start={}, end={}'.format(i, i*block_size, (i+1)*block_size))
+                downloader = HttpDownLoader(fg.get_opener(), url, i*block_size, (i+1)*block_size, name)
+                downloader.start()
+            else:
+                print('i={}, start={}, end={}'.format(i, i * block_size, length))
+                downloader = HttpDownLoader(fg.get_opener(), url, i*block_size, length, name)
+                downloader.start()
+
+
+
--- http_download.py
+++ http_download.py
@@ -11,19 +11,33 @@
 @desc: --
 """
 
+
+import os
 import threading
 
 
 class HttpDownLoader(threading.Thread):
 
-    def __init__(self, opener, start_pos, end_pos):
+    def __init__(self, opener, url, start_pos, end_pos, dest_file):
         threading.Thread.__init__(self)
         self.opener = opener
+        self.url = url
         self.start_pos = start_pos
         self.end_pos = end_pos
+        self.dest_file = dest_file
 
     def download(self):
-        pass
+        self.opener.addheaders = [('Range', 'bytes={}-{}'.format(self.start_pos, self.end_pos))]
+        url_handler = self.opener.open(self.url)
+        buffer_size = 1024
+        offset = self.start_pos
+        buffer = url_handler.read(buffer_size)
+        while buffer:
+            with open(self.dest_file, 'wb') as fh:
+                fh.seek(offset, os.SEEK_SET)
+                fh.write(buffer)
+                offset += len(buffer)
+                buffer = url_handler.read(buffer_size)
 
     def run(self):
         self.download()
